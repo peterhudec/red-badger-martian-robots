f88b34209bd285bbc5b96c084fa8ce2f6d39792b
Peter Hudec <peterhudec@peterhudec.com>
2023-03-01 17:44:41 +0000

    Implemented the computePath function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,78 @@
-const martianRobots = input =>
-  null
+const computePath = ({
+  path,
+  robotX,
+  robotY,
+  robotOrientation,
+  worldW,
+  worldH,
+  scented,
+}) => {
+  let orientation = robotOrientation
+  let x = robotX
+  let y = robotY
 
-module.exports = martianRobots
+  for (const move of path) {
+    let candidateX = x
+    let candidateY = y
+
+    if (move === 'F') {
+      switch (orientation) {
+        case 'N':
+          candidateY++
+          break
+        case 'S':
+          candidateY--
+          break
+        case 'E':
+          candidateX++
+          break
+        case 'W':
+          candidateX--
+          break
+      }
+
+      const candidateOutOfBounds
+        = candidateX < 0
+        || candidateY < 0
+        || candidateX > worldW
+        || candidateY > worldH
+
+      const ignore = scented[x]?.[y]
+
+      if (candidateOutOfBounds && !ignore) {
+        return {
+          x,
+          y,
+          orientation: 'LOST',
+          scented: {
+            [x]: {
+              [y]: true,
+            },
+          },
+        }
+      }
+
+      if (!ignore || !candidateOutOfBounds) {
+        x = candidateX
+        y = candidateY
+      }
+    } else {
+      orientation ={
+        N: {L: 'W', R: 'E'},
+        S: {L: 'E', R: 'W'},
+        E: {L: 'N', R: 'S'},
+        W: {L: 'S', R: 'N'},
+      }[orientation][move]
+    }
+  }
+
+  return {x, y, orientation}
+}
+
+const martianRobots = input => {
+}
+
+module.exports = {
+  computePath,
+  martianRobots,
+}
--- src/index.test.js
+++ src/index.test.js
@@ -1,17 +1,55 @@
 const sut = require('./index.js')
 
-const trimLineWhiteSpace = s =>
-  s.replaceAll(/^ +/gm, '')
+test('computePath', () => {
 
-test('', () => {
-  expect(sut(trimLineWhiteSpace(`
-    53
-    11E RFRFRFRF
-    32N FRRFLLFFRRFLL
-    03W LLFFFLFLFL
-  `))).toBe(trimLineWhiteSpace(`
-    11E
-    33NLOST
-    23S
-  `))
+  expect(sut.computePath({
+    robotOrientation: 'E',
+    robotX: 1,
+    robotY: 1,
+    worldW: 5,
+    worldH: 3,
+    path: 'RFRFRFRF',
+    scented: {},
+  })).toEqual({
+    x: 1,
+    y: 1,
+    orientation: 'E',
+  })
+
+  expect(sut.computePath({
+    robotOrientation: 'N',
+    robotX: 3,
+    robotY: 2,
+    worldW: 5,
+    worldH: 3,
+    path: 'FRRFLLFFRRFLL',
+    scented: {},
+  })).toEqual({
+    x: 3,
+    y: 3,
+    orientation: 'LOST',
+    scented: {
+      3: {
+        3: true,
+      },
+    },
+  })
+
+  expect(sut.computePath({
+    robotOrientation: 'W',
+    robotX: 0,
+    robotY: 3,
+    worldW: 5,
+    worldH: 3,
+    path: 'LLFFFLFLFL',
+    scented: {
+      3: {
+        3: true,
+      },
+    },
+  })).toEqual({
+    x: 2,
+    y: 3,
+    orientation: 'S',
+  })
 })
